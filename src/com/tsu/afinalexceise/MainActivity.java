fa2167c606117b0aa039e7f3315257b3ff84272f
tadeshan <tadeshanc@gmail.com>
2014-01-03 10:41:09 +0800

    测试自定义THttp成功

--- src/com/tsu/afinalexceise/MainActivity.java
+++ src/com/tsu/afinalexceise/MainActivity.java
@@ -1,24 +1,67 @@
 package com.tsu.afinalexceise;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.tsu.frame.THttp;
+import com.tsu.frame.THttp.HttpRequestCallBack;
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
+	private String TAG = "MainActivity";
+	private THttp tHttp;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		tHttp = new THttp();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	
+	public void onclick(View view){
+		
+		switch (view.getId()) {
+		case R.id.btn_thttp:
+			testThttp();
+			break;
 
+		}
+	}
+	
+	private void testThttp(){
+		//司考吧的公开接口来测试
+		tHttp.getHttpRequest("http://lawedu.duapp.com/getAndroidMZPL?mzpl=0&aid=0", new HttpRequestCallBack<JSONObject>() {
+
+			@Override
+			public void requestLoding(long total, long current) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void requestSuccess(JSONObject t) {
+				// TODO Auto-generated method stub
+				try {
+					Log.i(TAG,t.getString("caseAna"));
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			@Override
+			public void requestFailure(Exception e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+	
 }
 
 
--- src/com/tsu/frame/THttp.java
+++ src/com/tsu/frame/THttp.java
@@ -3,6 +3,7 @@ package com.tsu.frame;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.ParameterizedType;
 import java.security.PublicKey;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
@@ -14,11 +15,15 @@ import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
+import android.util.Log;
 
 /**
  * @author tadeshanc@gmail.com
@@ -36,13 +41,14 @@ public class THttp {
 
 	private HttpClient httpClient;
 	private HttpGet httpGet;
+	private String TAG = "THttp";
 	
 	//固定为2个线程
 	private final Executor executor = Executors.newFixedThreadPool(2);
 	
 	public THttp(){
 		//userAgent 可不设置
-		httpClient = AndroidHttpClient.newInstance(null);
+		httpClient = new DefaultHttpClient();
 	}
 	//getHttp 方法
 	
@@ -53,21 +59,52 @@ public class THttp {
 	}
 	
 	
+	private final int HTTP_SUCCESS = 200;
+	private final int HTTP_FAILURE = 100;
+	private final int HTTP_LOADING = 300;
 	private final class HttpHandler<T> extends AsyncTask<Object, Object, Object>{
 
+		private HttpRequestCallBack<T> callBack;
+		private Class<T> clazz ;
+		
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			Log.i(TAG,"执行之前");
+		}
+		
 		@Override
 		protected Object doInBackground(Object... params) {
 			// TODO Auto-generated method stub
 			try {
+				
+				Log.i(TAG,"执行开始");
+				callBack = (HttpRequestCallBack) params[0];
+
+				//这个地方，我们肯定要获取泛型的类型，以便在下面进行对应的操作，但是泛型类型获取，我现在没能做到，so..直接给class赋特定值
+				//想必afinal仍然是同样的问题
+				clazz = (Class<T>) JSONObject.class;
+				Log.i(TAG,"泛型获取："+clazz.getSimpleName());
+				
 				HttpResponse httpResponse = httpClient.execute(httpGet);
-				Object entity = jsonEntityHandler.handler(this,httpResponse);
-				publishProgress(HttpStatus.SC_OK,entity);
-			} catch (ClientProtocolException e) {
+				Log.i(TAG,"响应码："+httpResponse.getStatusLine().getStatusCode());
+				
+				if(httpResponse.getStatusLine().getStatusCode()==200){
+					Object entity = null;
+					if(clazz.getSimpleName().equals(JSONObject.class.getSimpleName())){
+						Log.i(TAG,"jsonbject 泛型判断");
+						entity = handlerJsonObjectEntity(httpResponse);
+					}else{
+						//...
+					}
+					
+					publishProgress(HTTP_SUCCESS,entity);
+				}
+				
+			}  catch (Exception e) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				Log.e(TAG,e.getMessage());
 			}
 			
 			return null;
@@ -78,32 +115,50 @@ public class THttp {
 		protected void onProgressUpdate(Object... values) {
 			// TODO Auto-generated method stub
 			super.onProgressUpdate(values);
-			int code = (Integer) values[0];
-			T t = (T) values[1];
-		}
-		
-		
-		private final class JSONObjectEntity{
+			int code = (Integer) values[0];		
+			switch (code) {
+			case HTTP_SUCCESS:
+				callBack.requestSuccess((T) values[1]);
+				break;
 
-			public JSONObject handler(HttpResponse httpResponse) throws Exception {
-				// TODO Auto-generated method stub
-				InputStream inputStream = httpResponse.getEntity().getContent();
-				ByteArrayOutputStream outSteam = new ByteArrayOutputStream();
-				byte[] buffer = new byte[1024];
-				int len = 0;
-				while( (len = inputStream.read(buffer)) !=-1 ){
-					outSteam.write(buffer, 0, len);
-				}
-				outSteam.close();
-				inputStream.close();
-				return new JSONObject(new String(outSteam.toByteArray()));
+			case HTTP_LOADING:
+				
+				break;
+			case HTTP_FAILURE:
+				
+				break;
 			}
-			
 		}
+		
+	
+		/**
+		 * 处理jsonArray的handler
+		 * @param httpResponse
+		 * @return
+		 * @throws Exception
+		 */
+		JSONObject handlerJsonObjectEntity(HttpResponse httpResponse) throws Exception {
+			// TODO Auto-generated method stub
+			InputStream inputStream = httpResponse.getEntity().getContent();
+			ByteArrayOutputStream outSteam = new ByteArrayOutputStream();
+			byte[] buffer = new byte[1024];
+			int len = 0;
+			while( (len = inputStream.read(buffer)) !=-1 ){
+				outSteam.write(buffer, 0, len);
+				//1.状态 2.进度,不再写了
+				//publishProgress(HTTP_LOADING,current);
+			}
+			String data = new String(outSteam.toByteArray());
+			outSteam.close();
+			inputStream.close();
+			return new JSONObject(data);
+		}
+		
+		
 	}
 	
 	
-	interface HttpRequestCallBack<T>{
+	public interface HttpRequestCallBack<T>{
 		
 		//1.loading
 		void requestLoding(long total,long current);
